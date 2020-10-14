a17b707bf28cd315cbe0a2d62351bb85967d7975
Noeliahr <noelia.hernandez.rodriguez30@gmail.com>
2020-10-14 20:49:48 +0200

    Cuarto Ejemplo de React llamado Cotizador de Seguros de Automoviles

---
+++ cotizador_de_seguros_de_automovil/src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from "react";
+import styled from "@emotion/styled";
+
+import Header from "./components/Header";
+import Formulario from "./components/Formulario";
+import Resumen from "./components/Resumen";
+import Resultado from "./components/Resultado";
+import Spinner from "./components/Spinner";
+
+const Contenedor = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const ContenedorFormulario = styled.div`
+  background-color: #fff;
+  padding: 3rem;
+`;
+
+function App() {
+  const [resumen, guardarResumen] = useState({
+    cotizacion: 0,
+    datos: {
+      marca: "",
+      year: "",
+      plan: "",
+    },
+  });
+
+  const [cargando, guardarCargando] = useState(false);
+
+  const { cotizacion, datos } = resumen;
+
+  return (
+    <Contenedor>
+      <Header titulo="Cotizador de Seguros" />
+      <ContenedorFormulario>
+        <Formulario
+          guardarResumen={guardarResumen}
+          guardarCargando={guardarCargando}
+        />
+
+        {cargando ? <Spinner /> : null}
+
+        {!cargando ? (
+          <React.Fragment>
+            <Resumen datos={datos} />
+            <Resultado cotizacion={cotizacion} />
+          </React.Fragment>
+        ) : null}
+      </ContenedorFormulario>
+    </Contenedor>
+  );
+}
+
+export default App;
